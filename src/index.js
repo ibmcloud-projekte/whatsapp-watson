aa3be11ba971d8048d6d7fe2277321b76c48b5d0
Diego Cassandri <diego.cassandri@gmail.com>
2020-09-07 21:49:48 -0300

    Start

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
+//whatson
+const AssistantV2 = require('ibm-watson/assistant/v2');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+const assistant = new AssistantV2({
+    authenticator: new IamAuthenticator({ apikey: process.env.WATSONASSISTANT_APIKEY }),
+    serviceUrl: 'https://gateway.watsonplatform.net/assistant/api/',
+    version: '2018-09-19'
+  });
+
 // Start the webapp
 const webApp = express();
 
@@ -22,6 +32,8 @@ webApp.get('/', (req, res) => {
 
 const WA = require('../helper-function/whatsapp-send-message');
 
+let session_id;
+
 // Route for WhatsApp
 webApp.post('/whatsapp', async (req, res) => {
 
@@ -31,8 +43,26 @@ webApp.post('/whatsapp', async (req, res) => {
     console.log(message);
     console.log(senderID);
 
-    // Write a function to send message back to WhatsApp
-    await WA.sendMessage('Hello from the other side.', senderID);
+    console.log(req.body);
+
+    const session = async (assistantId) => {
+        const response = await assistant.createSession({
+            assistantId: assistantId
+        })
+
+    return response.result.session_id;
+}
+    console.log(session_id);
+    if(!session_id) 
+         session_id = await session(process.env.WATSONASSISTANT_ID);
+
+    const response = await assistant.message({
+        input: { text: message },
+        assistantId: process.env.WATSONASSISTANT_ID,
+        sessionId: session_id,
+    });
+
+    WA.sendMessage(JSON.stringify(response.result.output.generic[0].text),senderID);
 
 });
 
